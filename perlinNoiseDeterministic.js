766dd150e7fc849a7fcd76c53c6e45cf1f37c92f
Keshav Madhav <keshav2552003@gmail.com>
2024-03-24 12:54:11 +0530

    Modified perlin noise functions

--- perlinNoiseDeterministic.js
+++ perlinNoiseDeterministic.js
@@ -1,4 +1,4 @@
-var p = new Array(512);
+var pDeterministic = new Array(512);
 var permutation = [151,160,137,91,90,15,
   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
@@ -13,36 +13,51 @@ var permutation = [151,160,137,91,90,15,
   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
 for (var i=0; i < 256 ; i++) 
-  p[256+i] = p[i] = permutation[i];
+  pDeterministic[256+i] = pDeterministic[i] = permutation[i];
 
-function fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-function lerp(t, a, b) { return a + t * (b - a); }
-function grad(hash, x, y, z) {
+function fadeDeterministic(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
+function lerpDeterministic(t, a, b) { return a + t * (b - a); }
+function gradDeterministic(hash, x, y, z) {
   var h = hash & 15;
   var u = h<8 ? x : y,
       v = h<4 ? y : h==12||h==14 ? x : z;
   return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
 }
 
-function perlinDeterministic(x, y, z) {
+function perlinDeterministic() {
+  var x = arguments[0] || 0;
+  var y = arguments[1] || 0;
+  var z = arguments[2] || 0;
+
   var X = Math.floor(x) & 255,
       Y = Math.floor(y) & 255,
       Z = Math.floor(z) & 255;
   x -= Math.floor(x);
   y -= Math.floor(y);
   z -= Math.floor(z);
-  var u = fade(x),
-      v = fade(y),
-      w = fade(z);
-  var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z,
-      B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;
+  var u = fadeDeterministic(x),
+      v = fadeDeterministic(y),
+      w = fadeDeterministic(z);
+  var A = pDeterministic[X  ]+Y, AA = pDeterministic[A]+Z, AB = pDeterministic[A+1]+Z,
+      B = pDeterministic[X+1]+Y, BA = pDeterministic[B]+Z, BB = pDeterministic[B+1]+Z;
 
-  return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),
-                                 grad(p[BA  ], x-1, y  , z   )),
-                       lerp(u, grad(p[AB  ], x  , y-1, z   ),
-                                 grad(p[BB  ], x-1, y-1, z   ))),
-                 lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),
-                                 grad(p[BA+1], x-1, y  , z-1 )),
-                       lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
-                                 grad(p[BB+1], x-1, y-1, z-1 ))));
+  var rawNoise;
+  if (arguments.length === 1) {
+    // 1D Perlin noise
+    rawNoise = lerpDeterministic(u, gradDeterministic(pDeterministic[AA  ], x, 0, 0), gradDeterministic(pDeterministic[BA  ], x-1, 0, 0));
+  } else if (arguments.length === 2) {
+    // 2D Perlin noise
+    rawNoise = lerpDeterministic(v, lerpDeterministic(u, gradDeterministic(pDeterministic[AA  ], x, y, 0), gradDeterministic(pDeterministic[BA  ], x-1, y, 0)),
+                        lerpDeterministic(u, gradDeterministic(pDeterministic[AB  ], x, y-1, 0), gradDeterministic(pDeterministic[BB  ], x-1, y-1, 0)));
+  } else {
+    // 3D Perlin noise
+    rawNoise = lerpDeterministic(w, lerpDeterministic(v, lerpDeterministic(u, gradDeterministic(pDeterministic[AA  ], x, y, z), gradDeterministic(pDeterministic[BA  ], x-1, y, z)),
+                              lerpDeterministic(u, gradDeterministic(pDeterministic[AB  ], x, y-1, z), gradDeterministic(pDeterministic[BB  ], x-1, y-1, z))),
+                        lerpDeterministic(v, lerpDeterministic(u, gradDeterministic(pDeterministic[AA+1], x, y, z-1), gradDeterministic(pDeterministic[BA+1], x-1, y, z-1)),
+                              lerpDeterministic(u, gradDeterministic(pDeterministic[AB+1], x, y-1, z-1), gradDeterministic(pDeterministic[BB+1], x-1, y-1, z-1))));
+  }
+
+  var noise = (rawNoise + 1) / 2;
+
+  return noise;
 }
