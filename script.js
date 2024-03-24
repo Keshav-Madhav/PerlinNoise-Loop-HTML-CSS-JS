dbef94341fcd0dd4e9de9d4f3e2d3c09b00dab95
Keshav Madhav <keshav2552003@gmail.com>
2024-03-24 12:19:04 +0530

    Added circle drawing with points

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function resizeCanvas() {
   canvas.height = window.innerHeight - 1;
 }
 resizeCanvas();
+
+
+// Function to continuously draw on canvas
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw a circle
+  ctx.beginPath();
+  ctx.strokeStyle = 'white';
+  for (let i = 0; i < Math.PI * 2; i += 0.01) {
+    let r = Math.random() * 30 + 100;
+    let x = r * Math.cos(i) + canvas.width / 2;
+    let y = r * Math.sin(i) + canvas.height / 2;
+    ctx.lineTo(x, y);
+  }
+  ctx.closePath();
+  ctx.stroke();
+  requestAnimationFrame(draw);
+}
+
+draw();
